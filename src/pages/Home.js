f50ddf56d76b66c0e565bbb849c02ea2b3aee145
jahidcs <csq.jahid@gmail.com>
2022-09-04 01:19:16 +0600

    bring out from component folder

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            Home
+            <hr />
+            <p>This is homepage</p>
+        </>
+    )
+}
+
+export default Home
