61d08cc17df27daa74648826b100226ca60e20cc
jahidcs <csq.jahid@gmail.com>
2022-09-04 01:14:51 +0600

    access token and protected the routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Contact from './components/pages/Contact'
-import Dashboard from './components/pages/Dashboard'
-import Home from './components/pages/Home'
-import Layout from './components/pages/Layout'
-import LoginReg from './components/pages/auth/LoginReg'
-import ResetPassword from './components/pages/auth/PasswordReset'
-import SendPasswordResetEmail from './components/pages/auth/SendPasswordResetEmail'
+import { useSelector } from 'react-redux'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Contact from './pages/Contact'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import LoginReg from './pages/auth/LoginReg'
+import ResetPassword from './pages/auth/PasswordReset'
+import SendPasswordResetEmail from './pages/auth/SendPasswordResetEmail'
 
 function App() {
+    const { access_token } = useSelector((state) => state.auth)
     return (
         <>
             <BrowserRouter>
@@ -15,11 +17,11 @@ function App() {
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
                         <Route path="contact" element={<Contact />} />
-                        <Route path="auth" element={<LoginReg />} />
+                        <Route path="auth" element={!access_token ? <LoginReg /> : <Navigate to="/dashboard" />} />
                         <Route path="forgotpass" element={<SendPasswordResetEmail />} />
                         <Route path="reset" element={<ResetPassword />} />
                     </Route>
-                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/dashboard" element={access_token ? <Dashboard /> : <Navigate to="/auth" />} />
                     <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
                 </Routes>
             </BrowserRouter>
